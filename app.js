23cc031437725157147922d259b9d44ffe255e47
credactor <175741800+credactor@users.noreply.github.com>
2024-11-14 19:11:07 +0300

    Try path resolve 4

--- app.js
+++ app.js
@@ -17,12 +17,13 @@ app.use(compression());
 //   origin:["https://space-images-library.onrender.com"]
 // }));
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', "https://space-images-library.onrender.com");
-  // res.setHeader('Access-Control-Allow-Origin', "http://localhost:5173");
+  // res.setHeader('Access-Control-Allow-Origin', "https://space-images-library.onrender.com");
+  res.setHeader('Access-Control-Allow-Origin', "http://localhost:5173");
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', true);
   // res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Content-Type', 'text/html');
   next();
 });
 
